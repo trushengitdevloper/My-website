45f0d3a3a9e6fd74f58f70176ee8bc1c0335c745
SOLANKI TRUSHEN <109092255+trushengitdevloper@users.noreply.github.com>
2023-08-21 13:24:35 +0530

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       resolve: "gatsby-theme-portfolio-minimal",
       options: {
-        siteUrl: "https://gatsby-starter-portfolio-minimal-theme.netlify.app/", // Used for sitemap generation
+        siteUrl: "https://solanki-trushen.netlify.app/", // Used for sitemap generation
         manifestSettings: {
           favicon: "./content/images/favicon.png", // Path is relative to the root
           siteName: "Solanki Trushen Portfolio", // Used in manifest.json
